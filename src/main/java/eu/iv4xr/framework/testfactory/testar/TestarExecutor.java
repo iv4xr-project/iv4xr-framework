72938e0a7b4e4bb833e4e8a48a8f38575ffac050
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2022-10-08 12:55:24 +0200

    Update TESTAR and SE dependencies

--- src/main/java/eu/iv4xr/framework/testfactory/testar/TestarExecutor.java
+++ src/main/java/eu/iv4xr/framework/testfactory/testar/TestarExecutor.java
@@ -12,9 +12,12 @@ public class TestarExecutor {
 		// TESTAR uses the windows.dll to use Windows native methods
 		FileUtils.copyFileToDirectory(new File("resources//windows10//windows.dll"), new File("."));
 
+		// TODO: The existence of the steam_appid.txt file allows the execution of the OpenCover tool. 
+		// But it is not invoking steam to launch SE with the iv4xr-plugin automatically
+
 		// Prepare the path that contains the TESTAR protocol to be executed
 		String settingsDir = "." + File.separator + "resources\\settings" + File.separator;
-		String protocolName = "se_commands_testar_navigate";
+		String protocolName = "se_testar_navigate";
 		String testSettingsFileName = settingsDir + protocolName + File.separator + "test.settings";
 		System.out.println("Test settings is <" + testSettingsFileName + ">");
 
